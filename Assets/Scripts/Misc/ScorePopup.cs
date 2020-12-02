28dedacdcdf766e7e51b3cfecc06549be56b6bc5
rlasne <robin.lasne@gmail.com>
2020-12-02 14:12:24 +0100

    Animated Score popup at every match

---
+++ Assets/Scripts/Misc/ScorePopup.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScorePopup : MonoBehaviour
+{
+    public float animDur;
+    public float animDist;
+    
+    TextMeshPro text;
+    
+    public void InitAnim(string content, Color inside, Color outside)
+    {
+        text = GetComponent<TextMeshPro>();
+
+        text.text = content;
+        text.color = inside;
+        text.outlineColor = outside;
+
+        StartCoroutine(Anim());
+    }
+
+    IEnumerator Anim()
+    {
+        Color startColor = text.color;
+        Color endColor = startColor;
+        endColor.a = 0;
+
+        Vector2 startPos = transform.position;
+        Vector2 endPos = startPos + Vector2.up*animDist;
+
+        for(float t = 0; t < 1; t += Time.deltaTime / animDur)
+        {
+            text.color = Color.Lerp(startColor, endColor, t);
+            transform.position = Vector2.Lerp(startPos, endPos, t);
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Linq;
 
 public class ScoreManager : MonoBehaviour
 {
-    public TMPro.TextMeshProUGUI scoreDisplay;
+    public TextMeshProUGUI scoreDisplay;
+    public ScorePopup popupPrefab;
 
     int currentScore=0;
 
@@ -16,9 +19,23 @@ public class ScoreManager : MonoBehaviour
     public void AddScore(List<CellMatch> matches)
     {
         int thisScore = 0;
-        foreach (var match in matches) thisScore += match.score;
+        foreach (var match in matches)
+        {
+            thisScore += match.score;
+
+            var popup = Instantiate(popupPrefab, match.getCenter(), Quaternion.identity);
+            popup.InitAnim(match.score.ToString(), Color.white, Color.black);
+        }
 
         currentScore += (thisScore * matches.Count + 1) / 2;
+
+        if (matches.Count > 1)
+        {
+            var center = matches.Aggregate(Vector3.zero, (e, v) => e + v.getCenter()/matches.Count);
+            var popup = Instantiate(popupPrefab, center, Quaternion.identity);
+            var message = string.Format("×{0}", .5f + .5f * matches.Count);
+            popup.InitAnim(message, Color.white, Color.black);
+        }
 
         scoreDisplay.text = currentScore.ToString();
     }
