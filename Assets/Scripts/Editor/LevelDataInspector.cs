e75e812003807dd1b2c3291352d446d2b0239143
rlasne <robin.lasne@gmail.com>
2020-12-06 07:37:55 +0100

    Start work on level editor

---
+++ Assets/Scripts/Editor/LevelDataInspector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LevelStartData))]
+public class LevelDataInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorUtility.SetDirty(target);
+
+        var _ = (LevelStartData)target;
+
+        if(_.hexagonRadius*2+1 != _.cells.Length)
+        {
+            _.cells = new intArray[_.hexagonRadius * 2 + 1];
+            for(int i=0; i < _.hexagonRadius * 2 + 1; ++i)
+            {
+                _.cells[i].array = new int[_.cells.Length - Mathf.Abs(_.hexagonRadius - i)];
+            }
+        }
+
+        for(int i=0; i < _.cells.Length; ++i)
+        {
+            using(new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.Space((2*_.hexagonRadius-_.cells[i].array.Length) * 15);
+                for(int j=0; j < _.cells[i].array.Length;++j)
+                {
+                    using(var scope = new EditorGUI.ChangeCheckScope())
+                    {
+                        _.cells[i][j] = EditorGUILayout.IntField(_.cells[i][j], GUILayout.Width(30));
+
+                        if (scope.changed) Undo.RecordObject(_, "Change grid content");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LevelStartData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct intArray {
+    public int[] array;
+
+    public int this[int i] {
+        get {
+            return array[i];
+        }
+        set {
+            array[i] = value;
+        }
+    }
+}
+
+[CreateAssetMenu(menuName ="ScriptableObjects/Level Data")]
+public class LevelStartData : ScriptableObject
+{
+    public int hexagonRadius;
+    [HideInInspector]
+    public intArray[] cells;
+}
