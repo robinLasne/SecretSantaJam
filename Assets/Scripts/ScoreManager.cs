c62b8364f6c0054c6f61b1222d6eeedaa495b6ca
rlasne <robin.lasne@gmail.com>
2020-12-15 19:53:00 +0100

    Saving score all the time to avoid loss

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -56,6 +56,8 @@ public class ScoreManager : MonoBehaviour
         
 
         scoreDisplay.text = currentScore.ToString();
+
+        PlayerPrefs.SetInt("score", overallScore+currentScore);
     }
     #endregion
 
