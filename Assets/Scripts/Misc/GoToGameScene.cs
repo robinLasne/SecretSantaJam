7609e9b9d891f73b59bcb19a3fd86c2720354619
rlasne <robin.lasne@gmail.com>
2020-12-15 15:22:03 +0100

    More main menu pics, title screen, splash screen

---
+++ Assets/Scripts/Misc/GoToGameScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToGameScene : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
