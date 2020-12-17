5313b5c82a8ac4ab55d1ff2fa36734cb26345a9d
rlasne <robin.lasne@on-x.com>
2020-12-17 22:06:59 +0100

    Start programming book

---
+++ Assets/Scripts/ManualBook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualBook : MonoBehaviour
+{
+	public Transform pagesParent;
+
+	int curPage;
+	private void OnEnable() {
+
+		foreach (Transform page in pagesParent) page.gameObject.SetActive(false);
+
+		pagesParent.GetChild(0).gameObject.SetActive(true);
+		curPage = 0;
+	}
+
+	public void NextPage() {
+		if(curPage < pagesParent.childCount - 1) {
+			pagesParent.GetChild(curPage).gameObject.SetActive(false);
+			curPage++;
+			pagesParent.GetChild(curPage).gameObject.SetActive(true);
+		}
+	}
+
+	public void PrevPage() {
+		if (curPage > 0) {
+			pagesParent.GetChild(curPage).gameObject.SetActive(false);
+			curPage--;
+			pagesParent.GetChild(curPage).gameObject.SetActive(true);
+		}
+	}
+}
